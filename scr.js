121740d264b20c09660219926c1895c3e70f2b19
youjunho613 <njn613@gmail.com>
2023-06-02 12:09:53 +0900

    선택 사항(추가 기능)&코드정리
    제출 전 코드정리
    언어 변경 함수 및 HTML 버튼 구현
    페이지 변경 함수 및 HTML a, input 구현

--- scr.js
+++ scr.js
@@ -9,34 +9,84 @@ const options = {
 
 let movieUrl =
   "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+let inputPage = Number(document.getElementById("inputPage").value);
+
+const changeKo = () => {
+  return (movieUrl = movieUrl.replace("en", "ko")), getMovies();
+};
+
+const changeEn = () => {
+  return (movieUrl = movieUrl.replace("ko", "en")), getMovies();
+};
+
+const pageUp = () => {
+  if (inputPage < 500) {
+    inputPage++;
+    movieUrl = movieUrl.substring(0, 65) + inputPage;
+    return (
+      getMovies(), (document.getElementById("inputPage").value = inputPage)
+    );
+  }
+};
+
+const searchPage = () => {
+  movieUrl =
+    movieUrl.substring(0, 65) + document.getElementById("inputPage").value;
+  if (1 <= document.getElementById("inputPage").value <= 500) {
+    return setTimeout(function () {
+      getMovies(), (inputPage = document.getElementById("inputPage").value);
+    }, 1000);
+  }
+  // else if (
+  //   document.getElementById("inputPage").value < 1 &&
+  //   document.getElementById("inputPage").value > 500
+  // ) {
+  //   return setTimeout(function () {
+  //     document.getElementById("inputPage").value = 1;
+  //     movieUrl =
+  //       movieUrl.substring(0, 65) + document.getElementById("inputPage").value;
+  //     getMovies();
+  //   }, 1000);
+  // }
+};
+
+const pageDown = () => {
+  if (inputPage > 1) {
+    inputPage--;
+    movieUrl = movieUrl.substring(0, 65) + inputPage;
+    return (
+      getMovies(), (document.getElementById("inputPage").value = inputPage)
+    );
+  }
+};
 
 // fetch 값 Promise 에 저장
-let fetchMovies = async () => {
+const fetchMovies = async () => {
   const response = await fetch(movieUrl, options);
   const jsonData = await response.json();
   return jsonData.results;
 };
 
 // 영화 fetch [GET] => innerHTML에 붙여주기
-// (화살표 함수, 비구조화 할당)
-// forEach, getElementById, innerHTML
-let getMovies = async () => {
+const getMovies = async () => {
   let movies = await fetchMovies();
-
+  let movieContainer = document.getElementById("movieContainer");
+  movieContainer.innerHTML = "";
   movies.forEach(({ title, overview, poster_path, vote_average, id }) => {
     let movieCard = `<div class="movie-card" id="${id}" onclick="alertId(event)">
         <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}"/>
         <h5 class="movie-title">${title}</h5>
         <p class="movie-overview">${overview}</p>
         <p class="movie-vote">Rating: ${vote_average}</p></div>`;
-    document.getElementById("movieContainer").innerHTML += movieCard;
+    movieContainer.innerHTML += movieCard;
   });
 };
 getMovies();
 
 // 검색 함수
-let searchFunc = (event) => {
+const searchFunc = (event) => {
   event.preventDefault();
+  console.log(searchBtn);
   let write = document.getElementById("searchInput").value.toUpperCase();
   let item = document.getElementsByClassName("movie-card");
   for (let i = 0; i < item.length; i++) {
@@ -63,26 +113,31 @@ searchInput.addEventListener("keypress", (pressKey) => {
 
 // 영화 카드 클릭 => alert "영화 id"
 // try.1 실패 이유 : 이벤트 버블링
-function alertId(event) {
-  // let trackingId = event.target.id; // 문제!
+// let trackingId = event.target.id; // 문제!
+alertId = (event) => {
   let trackingId = event.currentTarget.id; // 해결!
   alert(`영화 ID : ${trackingId}`);
-}
+};
 
 // try.2 [실패] HTML<onclick> 삭제 후 .addEventListener 로 동작시키기
+// fetch >> .movie-card 선택 >> id 값 호출 >> alret(${ID})
 // let alert = async () => {
 //   await getMovies();
+//   await fetchMovies();
 //   let a = document.querySelectorAll(".movie-card");
+//   let a = document.querySelector(".movie-card");
 //   console.log(a);
-//   a.addEventListener("click", () => alert);
 // };
-// let a = document.querySelectorAll(".movie-card");
+// a.addEventListener("click", async () => {
+//   await getMovies();
+//   alert;
+// });
 // a.addEventListener("click", () => alert);
 
 // input 창 자동 커서
-function inputFocus() {
+inputFocus = () => {
   document.getElementById("searchInput").focus();
-}
+};
 onload = inputFocus();
 
 // inputFocus() 함수를 3초마다 반복
@@ -91,9 +146,4 @@ onload = inputFocus();
 //     inputFocus();
 //   }, 3000);
 // }
-// goInfinite(); // 3초 마다 inputFocus() 함수 반복
-
-// commit 쓸 변경사항
-// button 태그 onclick 검색 기능을 바닐라 자바스크립트 addEventListener로 구현
-// input 태그 onkeypress 엔터키 검색 기능을 바닐라 자바스크립트 addEventListener로 구현
-// input 태그 autofocus 기능을 자바스크립트 메서드로 구현
+// goInfinite();
